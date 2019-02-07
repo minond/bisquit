a506e6035be5807402a1d79432c4b17b1b3bec3d
Marcos Minond <minond.marcos@gmail.com>
2019-02-06 21:26:28 -0700

    passing previously parsed expression around to calculate real EOFs, nicer invalid expression handling

--- Parser.scala
+++ Parser.scala
@@ -15,46 +15,39 @@ object Parser {
         }
 
   def parseCond(start: Token, tail: Iterator[Token]): Expr = {
-    val cond = next(tail)
-    if (!cond.isInstanceOf[Expr]) {
-      // TODO expecting expr but no way of stating that, empty for now
-      return InvalidExpr(List.empty, List(cond))
-    }
-    eat(Identifier.word("then"), tail) match {
+    val cond = next(start, tail)(return _)
+    val keyword1 = eat(cond, Identifier.word("then"), tail) match {
       case (false, got) =>
         return InvalidExpr(List(got), List(Identifier.word("then")))
-      case (true, _) =>
+      case (true, got) => got
     }
 
-    val pass = next(tail)
-    if (!pass.isInstanceOf[Expr]) {
-      // TODO expecting expr but no way of stating that, empty for now
-      return InvalidExpr(List.empty, List(pass))
-    }
-    eat(Identifier.word("else"), tail) match {
+    val pass = next(keyword1, tail)(return _)
+    val keyword2 = eat(pass, Identifier.word("else"), tail) match {
       case (false, got) =>
         return InvalidExpr(List(got), List(Identifier.word("else")))
-      case (true, _) =>
+      case (true, got) => got
     }
 
-    val fail = next(tail)
-    if (!fail.isInstanceOf[Expr]) {
-      // TODO expecting expr but no way of stating that, empty for now
-      return InvalidExpr(List.empty, List(fail))
-    }
-
+    val fail = next(keyword2, tail)(return _)
     Cond(cond, pass, fail, start.getStart)
   }
 
-  def next(toks: Iterator[Token]): Expr =
+  def next(last: Token, toks: Iterator[Token])(
+      handler: InvalidExpr => InvalidExpr
+  ): Expr =
     if (!toks.hasNext)
-      InvalidExpr(List(EOF("", -1)), List.empty)
+      handler(InvalidExpr(List(EOF(last.getFile, last.getEnd)), List.empty))
     else
       parse(toks).next
 
-  def eat(expecting: Token, toks: Iterator[Token]): (Boolean, Token) =
+  def eat(
+      last: Token,
+      expecting: Token,
+      toks: Iterator[Token]
+  ): (Boolean, Token) =
     if (!toks.hasNext)
-      (false, EOF("", -1))
+      (false, EOF(last.getFile, last.getEnd))
     else {
       val got = toks.next
       (Lexer.eq(got, expecting), got)
