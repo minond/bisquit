165b0add3bcff55e67b7878a8631675812c47ab8
Marcos Minond <minond.marcos@gmail.com>
2019-02-21 23:36:03 -0700

    grammar docs

--- src/main/scala/xyz/minond/bisquit/node/Definitions.scala
+++ src/main/scala/xyz/minond/bisquit/node/Definitions.scala
@@ -1,5 +1,55 @@
 package xyz.minond.bisquit.node
 
+/* The language grammar:
+ *
+ *  main = { expr | stmt } ;
+ *
+ *  stmt = binding
+ *       ;
+ *
+ *  expr = unary-expr
+ *       | binary-expr
+ *       | match-expr
+ *       | cond-expr
+ *       | let-expr
+ *       | app-expr
+ *       | scalar
+ *       | id
+ *       ;
+ *
+ *  unary-expr = op scalar ;
+ *  binary-expr = scalar op scalar ;
+ *  match-expr = expr "match" match-case { match-case } ;
+ *  match-case = "|" expr "=>" expr ;
+ *  cond-expr = "if" expr "then" expr "else" expr ;
+ *  let-expr = "let" binding { binding } "in" expr ;
+ *  app-expr = id , "(" [ expr { "," expr } ] ")" ;
+ *
+ *  binding = ( var-decl | fun-decl ) "=" expr ;
+ *  var-decl = "val" arg-decl
+ *  arg-decl = id [ typ-decl ] ;
+ *  typ-decl = ":" id ;
+ *  fun-decl = "fun" id "(" [ arg-decl { arg-decl } ] ")" typ-decl ;
+ *
+ *  scalar = number | bool ;
+ *  bool = "true" | "false" ;
+ *  id = letter , { letter | digit | "_" } ;
+ *  string = '"' { ? } '"' ;
+ *
+ *  number = [ "-" ] ( number-dec | number-bin | number-hex ) ;
+ *  number-dec = digit , [ "." ] , { digit } ;
+ *  number-bin = "0b" , number-bin-opt , { number-bin-opt } ;
+ *  number-hex = "0x" , number-hex-opt , { number-hex-opt } ;
+ *  number-bin-opt = "0" | "1" ;
+ *  number-hex-opt = digit | "A" | "B" | "C" | "D" | "E" | "F" ;
+ *
+ *  op     = "+" | "-" | "*" | "&" | "^" | "%" | "!" | "\" | "|" | ">" | "<" ;
+ *  digit  = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
+ *  letter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L"
+ *         | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X"
+ *         | "Y" | "Z" ;
+ */
+
 abstract class Positioned(file: String, start: Int, end: Int) {
   def getFile = file
   def getStart = start
