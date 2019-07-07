7fcc2ac5d340a1270de4ad5f24ff3ac9a1da4c6e
Marcos Minond <minond.marcos@gmail.com>
2019-07-07 14:20:49 -0600

    updating grammar note

--- Expr.scala
+++ Expr.scala
@@ -1,53 +1,60 @@
 package xyz.minond.bisquit
 
-/* The language grammar:
+/* main = { expr | stmt }
+ *      ;
  *
- *  main = { expr | stmt } ;
+ * stmt = binding-stmt
+ *      ;
  *
- *  stmt = binding
- *       ;
+ * binding-stmt = func-binding-stmt
+ *              | val-binding-stmt
+ *              ;
  *
- *  expr = unary-expr
- *       | binary-expr
- *       | match-expr
- *       | cond-expr
- *       | let-expr
- *       | app-expr
- *       | scalar
- *       | id
- *       ;
+ * func-binding-stmt = "func" id "(" [ func-args ] ")" "=" expr
+ *                   ;
  *
- *  unary-expr = op scalar ;
- *  binary-expr = scalar op scalar ;
- *  match-expr = expr "match" match-case { match-case } ;
- *  match-case = "|" expr "=>" expr ;
- *  cond-expr = "if" expr "then" expr "else" expr ;
- *  let-expr = "let" binding { binding } "in" expr ;
- *  app-expr = id , "(" [ expr { "," expr } ] ")" ;
+ * func-args = op-typed-id { "," op-typed-id }
+ *           ;
  *
- *  binding = ( var-decl | func-decl ) "=" expr ;
- *  var-decl = "val" arg-decl
- *  arg-decl = id [ typ-decl ] ;
- *  typ-decl = ":" id ;
- *  func-decl = "func" id "(" [ arg-decl { arg-decl } ] ")" typ-decl ;
+ * typed-id = id [ ":" type-id ]
+ *          ;
  *
- *  scalar = number | bool ;
- *  bool = "true" | "false" ;
- *  id = letter , { letter | digit | "_" } ;
- *  string = '"' { ? } '"' ;
+ * val-binding-stmt = "val" typed-id "=" expr
+ *                  ;
  *
- *  number = [ "-" ] ( number-dec | number-bin | number-hex ) ;
- *  number-dec = digit , [ "." ] , { digit } ;
- *  number-bin = "0b" , number-bin-opt , { number-bin-opt } ;
- *  number-hex = "0x" , number-hex-opt , { number-hex-opt } ;
- *  number-bin-opt = "0" | "1" ;
- *  number-hex-opt = digit | "A" | "B" | "C" | "D" | "E" | "F" ;
+ * expr = cond-expr
+ *      | let-expr
+ *      | app-expr
+ *      | scalar
+ *      | id
+ *      ;
  *
- *  op     = "+" | "-" | "*" | "&" | "^" | "%" | "!" | "\" | "|" | ">" | "<" ;
- *  digit  = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
- *  letter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L"
- *         | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X"
- *         | "Y" | "Z" ;
+ * cond-expr = "if" expr "then" expr "else" expr
+ *           ;
+ *
+ * let-expr = "let" binding-expr "in" expr
+ *          ;
+ *
+ * app-expr = ( id | app-expr ) "(" [ expr { "," expr } ] ")"
+ *          ;
+ *
+ * scalar = number
+ *        | boolean
+ *        | string
+ *        ;
+ *
+ * id = ?? identifier ??
+ *    ;
+ *
+ * number = ?? number ??
+ *        ;
+ *
+ * boolean = "true"
+ *         | "false"
+ *         ;
+ *
+ * string = ?? string ??
+ *        ;
  */
 
 abstract class Positioned(file: String, start: Int, end: Int) {
