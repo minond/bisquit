70827ef97f9aed1abb4edb64af3f9ceedd92fdcb
Marcos Minond <minond.marcos@gmail.com>
2020-08-05 22:43:01 -0600

    formatting tokens

--- Token.scala
+++ Token.scala
@@ -6,28 +6,15 @@ sealed trait Token extends Positioned
 sealed trait Expression extends Token
 sealed trait Value extends Expression
 
-case object Eof
-  extends Token
+case object Eof extends Token
+case class Id(lexeme: String) extends Expression
+case class Binop(op: Id, left: Expression, right: Expression) extends Expression
+case class Uniop(op: Id, subject: Expression) extends Expression
+case class App(fn: Id | Func, args: List[Expression]) extends Expression
+case class Num(value: Double) extends Value
 
-case class Id(lexeme: String)
-  extends Expression
-
-case class Binop(op: Id, left: Expression, right: Expression)
-  extends Expression
-
-case class Uniop(op: Id, subject: Expression)
-  extends Expression
-
-case class App(fn: Id | Func, args: List[Expression])
-  extends Expression
-
-case class Num(value: Double)
-  extends Expression
-  with Value
-
-case class Func(params: List[Id], body: Expression)
-  extends Expression
-  with Value {
+case class Func(params: List[Id], body: Expression) extends Value {
   def curried(bindings: List[Value]) =
-    Func(params.drop(bindings.size), App(Func(params.take(bindings.size), body), bindings))
+    Func(params=params.drop(bindings.size),
+         body=App(Func(params.take(bindings.size), body), bindings))
 }
