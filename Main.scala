577420cfa04e2a13fe43e12859cc511b1fb7fd78
Marcos Minond <minond.marcos@gmail.com>
2020-08-05 20:30:06 -0600

    testing scoping

--- Main.scala
+++ Main.scala
@@ -1,33 +1,19 @@
 package xyz.minond.bisquit
 
 import xyz.minond.bisquit.token._
-import xyz.minond.bisquit.input.Positioned
 import xyz.minond.bisquit.runtime.Evaluator.eval
 
 object Main {
   def main(args: Array[String]): Unit =
-    // val s1 = Binop(Id("-"),
-    //                Id("a"),
-    //                Id("b"))
-    //
-    // val scope = Map("a" -> Num(22),
-    //                 "b" -> Num(20))
-    // println(s1)
-    // println(eval(s1, scope))
-    // println(eval(Uniop(Id("!"), Id("a")), scope))
-    // println(eval(Uniop(Id("-"), Id("a")), scope))
-    // println(Positioned.stdin(Id("hi"), 0, 0).position.get.toString)
+    val addIt = Func(List(Id("a"), Id("b"), Id("c")),
+                     Binop(Id("+"),
+                           Binop(Id("+"), Id("a"), Id("b")),
+                           Binop(Id("+"), Id("c"), Id("x"))))
 
-    // val add3 = Func(List(Id("a"), Id("b"), Id("c")),
-    val add3 = Func(List(Id("a"), Id("b"), Id("c")),
-                    Binop(Id("+"),
-                          Binop(Id("+"), Id("a"), Id("b")),
-                          Id("c")))
+    val scope = Map("addIt" -> addIt,
+                    "x" -> Num(32))
 
-    val scope = Map("add3" -> add3,
-                    "num" -> Num(32))
-
-    var expr = App(Id("num"),
+    var expr = App(Id("addIt"),
                    List(Binop(Id("+"), Num(32), Num(53)),
                         Num(34),
                         Num(65)))
