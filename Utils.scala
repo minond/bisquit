9cb1c278e28645a13a3bb59cc37ee82207c56646
Marcos Minond <minond.marcos@gmail.com>
2020-08-05 20:01:19 -0600

    tracking Utils file

---
+++ Utils.scala
@@ -0,0 +1,26 @@
+package xyz.minond.bisquit.utils
+
+import scala.reflect.ClassTag
+
+def ensure[V, L, R: ClassTag](value: V, left: => L): Either[L, R] =
+  value match {
+    case ok : R => Right(ok)
+    case _ => Left(left)
+  }
+
+object Eithers {
+  import scala.language.implicitConversions
+
+  implicit def listOfEithersToEithers[L, R](eithers: List[Either[L, R]]): Container[L, R] =
+    Container(eithers)
+
+  class Container[L, R](val eithers: List[Either[L, R]]) {
+    def squished(): Either[L, List[R]] = {
+      val acc: Either[L, List[R]] = Right(List())
+      eithers.foldLeft(acc) {
+        (acc, x) =>
+          acc.flatMap(xs => x.map(_ +: xs))
+      }
+    }
+  }
+}
