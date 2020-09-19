de0b417c5255ac5688cbebf963a2cc04dbe7ba14
Marcos Minond <minond.marcos@gmail.com>
2020-09-19 10:16:40 -0600

    type and ir repl modes

--- Repl.scala
+++ Repl.scala
@@ -10,16 +10,20 @@ import typechecker._
 
 import java.io._
 
+
+enum Mode {
+  case Parse
+  case Ir
+  case Type
+  case Eval
+}
+
+
 class Repl(
     var scope: RuntimeScope,
     in: InputStream = System.in,
     out: PrintStream = System.out,
 ) {
-  enum Mode {
-    case Parse
-    case Eval
-  }
-
   val promptPrefix = ""
   val promptStart = s"${promptPrefix}> "
   val promptCont = s"${" " * promptPrefix.size}| "
@@ -33,7 +37,7 @@ class Repl(
 
   def run(): Unit =
     while (true) {
-      print(if (buff.isEmpty) promptStart else promptCont)
+      out.print(if (buff.isEmpty) promptStart else promptCont)
 
       buff.append(reader.readLine()).toString match {
         case "" =>
@@ -53,31 +57,36 @@ class Repl(
           out.println("")
         }
 
+      case (Mode.Ir, code) =>
+        parseIt(code) { expr =>
+          out.println(pass1(expr))
+          out.println("")
+        }
+
+      case (Mode.Type, code) =>
+        parseIt(code) { expr =>
+          typeIt(expr) { (_, ty) =>
+            out.println(s": ${formatted(ty)}")
+            out.println("")
+          }
+        }
+
       case (Mode.Eval, code) =>
-        parseIt(code)(doIt)
-    }
-
-  def doIt(expr: Expression) =
-    val ir = pass1(expr)
-    infer(ir, scope, subs) match {
-      case Left(err) =>
-        out.println(s"type error: $err")
-        out.println("")
-
-      case Right(ty) =>
-        eval(ir, scope) match {
-          case Left(err) =>
-            out.println(s"runtime error: $err")
-            out.println("")
-          case Right(value) =>
-            out.println(s"= ${formatted(value, lvl = 3, short = true)} : ${formatted(ty)}")
-            out.println("")
+        parseIt(code) { expr =>
+          typeIt(expr) { (_, ty) =>
+            evalIt(expr) { value =>
+              out.println(s"= ${formatted(value, lvl = 3, short = true)} : ${formatted(ty)}")
+              out.println("")
+            }
+          }
         }
     }
 
   def parseIt(code: String)(ok: Expression => Unit): Boolean = {
     for res <- parse(code, fileName) do
       res match {
+        case Right(expr) => ok(expr)
+
         case Left(_: UnexpectedEOF) =>
           return false
 
@@ -85,17 +94,37 @@ class Repl(
           out.println(s"parse error: $err")
           out.println("")
           return true
-
-        case Right(expr) =>
-          ok(expr)
       }
 
     true
   }
 
+  def typeIt(expr: Expression)(ok: (Expression, Type) => Unit) = {
+    val ir = pass1(expr)
+    infer(ir, scope, subs) match {
+      case Right(ty) => ok(expr, ty)
+
+      case Left(err) =>
+        out.println(s"type error: $err")
+        out.println("")
+    }
+  }
+
+  def evalIt(expr: Expression)(ok: Value => Unit) =
+    val ir = pass1(expr)
+    eval(ir, scope) match {
+      case Right(value) => ok(value)
+
+      case Left(err) =>
+        out.println(s"runtime error: $err")
+        out.println("")
+    }
+
   def mode(code: String): (Mode, String) =
     (code.split(" ").toList) match {
       case (":parse" :: rest) => (Mode.Parse, rest.mkString(" "))
+      case (":type" :: rest) => (Mode.Type, rest.mkString(" "))
+      case (":ir" :: rest) => (Mode.Ir, rest.mkString(" "))
       case _ => (Mode.Eval, code)
     }
 }
