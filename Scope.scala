5558eeac61b17940232ff1440bdba153fa8b7bc1
Marcos Minond <minond.marcos@gmail.com>
2020-09-19 11:40:10 -0600

    removing unused typescope

--- Scope.scala
+++ Scope.scala
@@ -1,23 +1,12 @@
 package bisquit
 package scope
 
-import ast.{Builtin, Expression, Value}
+import ast._
 import typechecker.Type
 
 type RuntimeScope = Map[String, Value]
-type TypeScope = Map[String, Type]
 type Environment = Map[String, Expression]
 
 object Environment {
   def apply(): Environment = Map()
 }
-
-
-def typeScope(scope: RuntimeScope): TypeScope =
-  scope.foldLeft[TypeScope](Map()) {
-    case (acc, (label, value)) =>
-      value match {
-        case Builtin(signature, _) => acc ++ Map(label -> signature)
-        case _ => acc
-      }
-  }
